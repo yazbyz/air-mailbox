b55c59a2605138301cf75c16e6d7ba6e2a08a6d1
Hyejin Eom <yazbyz@gmail.com>
2020-12-20 23:31:42 +0900

    Apply .gitignore

---
+++ src/main/java/io/yazbyz/toypj/airmailbox/AirMailboxApplication.java
@@ -0,0 +1,13 @@
+package io.yazbyz.toypj.airmailbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirMailboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirMailboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yazbyz/toypj/airmailbox/AirMailboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yazbyz.toypj.airmailbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirMailboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
